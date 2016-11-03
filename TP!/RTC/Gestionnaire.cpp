d3319bdb2e6e88663956e7be5a437ec39bf61190
Maxime Verret <maxime.verret.3@ulaval.ca>
2016-11-03 12:41:03 -0400

    Gestionnaire.cpp 3/11 12h40

--- TP!/RTC/Gestionnaire.cpp
+++ TP!/RTC/Gestionnaire.cpp
@@ -1,4 +1,43 @@
+#include "Gestionnaire.h"
+#include "Auxiliaires.h"
+#include <map>
+using namespace std;
+
 Gestionnaire::Gestionnaire(std::string chemin_dossier) {
+
+	vector<vector<string>> resultats;
+	map<int, Ligne> m_lignes;
+	map<int, Station> m_stations;
+	map<string, Voyage> m_voyages;
+	map<Voyage*, Date> m_voyages_date;
+
+	lireFichier(chemin_dossier+"routes.txt",resultats, ';', 1 );
+
+	for (vector<vector<string>>::iterator itr = resultats.begin(); itr != resultats.end(); itr++)
+	{
+		Ligne maLigne(*itr);
+		m_lignes[maLigne.getId()]=maLigne;
+	}
+
+	lireFichier(chemin_dossier+"stops.txt",resultats, ';', 1 );
+
+	for (vector<vector<string>>::iterator itr = resultats.begin(); itr != resultats.end(); itr++)
+	{
+		Station maStation(*itr);
+		m_stations[maStation.getId()]=maStation;
+	}
+
+	lireFichier(chemin_dossier+"trips.txt",resultats, ';', 1 );
+
+	for (unsigned int i = 0;i<resultats.size(); i++)
+	{
+		Voyage monVoyage(resultats[i] ,&(m_lignes[stoi(resultats[i][0])]));
+		m_voyages[monVoyage.getId()]=monVoyage;
+	}
+
+	lireFichier(chemin_dossier+"calendar_dates.txt",resultats, ';', 1 );
+
+
 }
 
 bool Gestionnaire::date_est_prise_en_charge(const Date& date) {
